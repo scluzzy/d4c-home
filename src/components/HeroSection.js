f89ecad34b00cca22d1bfe403b87edc86b4bf190
scluzzy <3004sar2001@gmail.com>
2021-04-08 17:59:21 +0300

    video 2

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,7 +7,7 @@ import './HeroSection.css';
 function HeroSection() {
     return (
         <div className='hero-container'>
-            <video src="/videos/video-1.mp4" autoPlay loop muted />
+            <video src="/videos/video-2.mp4" autoPlay loop muted />
                 <h1>Connect To The World!</h1>
                 <p>Get started</p>
                 <div className="hero-btns">
